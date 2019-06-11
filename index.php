e4907888aa7e9ca80dd53bfdc8500e0d4d227118
BartoloPrime <51661235+BartoloPrime@users.noreply.github.com>
2019-06-11 10:14:38 -0500

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -11,17 +11,17 @@
 <body>
    <section class="wrapper">
     <header>
-      <h1 class="logo"><a href="index.html">Evaluación de u3</a></h1>
+      <h1 class="logo"><a href="index.php">Evaluación de u3</a></h1>
       <nav>
       <ul>
-      <li><a href="index.html" class="current">Inicio</a></li>
-      <li><a href=basicoJS.html>Básica</a></li>
+      <li><a href="index.php" class="current">Inicio</a></li>
+      <li><a href=basicoJS.php>Básica</a></li>
       </ul>
       </nav>
     </header>
     <section id="container">
-           <h1>Solución de problemas de Física</h1>
-
+           <h2>Solución de problemas de Física</h1>
+           <h1>Hallar la velocidad angular de un satelite artificial de la tierra que gira siguiendo una orbita circular con periodo de revolucion de 88 minutos</h2>
     </section>
     <footer class="pie">
      Creative Commons versión 3.0 SciSoft 2019
