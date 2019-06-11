33897770aede93e9a6d77a67180e05d89a66b564
BartoloPrime <51661235+BartoloPrime@users.noreply.github.com>
2019-06-11 12:07:37 -0500

    Update solucion.js

--- php7/solucion.js
+++ php7/solucion.js
@@ -1,12 +1,10 @@
- 
-        function calcula_densidad(){
-        var T=88;
+      function calcula_densidad(){
+        var Ti=88;
         //var radio= 6378e3;
         //var volumen= (4/3)*Math.PI*radio*radio*radio;
         //var densidad= masa/volumen;
-        var W=(2*Math.PI)/T;
+        var Wa=(2*Math.PI)/T;
         var d=document.getElementById("resultadoA");
-        d.innerHTML='El resultado es: 'W+' rad/seg';
+        d.innerHTML='El resultado es: 'Wa+' rad/seg';
         }
-
     
